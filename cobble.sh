ff41e9d63b5abd2dfd449f826f9edbf03a5c07d8
headmelted <headmelted@gmail.com>
2017-06-09 14:39:28 +0000

    Adding native architecture libx11-dev and libxkbfile-dev

--- cobble.sh
+++ cobble.sh
@@ -82,4 +82,5 @@ rpm graphicsmagick libwww-perl libxml-libxml-perl libxml-sax-expat-perl \
 dpkg-dev perl libconfig-inifiles-perl libxml-simple-perl \
 liblocale-gettext-perl libdpkg-perl libconfig-auto-perl \
 libdebian-dpkgcross-perl ucf debconf dpkg-cross tree \
+libx11-dev libxkbfile-dev \
 zlib1g-dev qemu binfmt-support qemu-user-static ${cobbler_packages_to_install};
