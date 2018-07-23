33103eb58d4bbd7dc1a83e7c53484f77c4b262ac
Jay Rodgers <headmelted@gmail.com>
2018-07-23 15:13:16 +0100

    Update cobble.sh

--- cobble.sh
+++ cobble.sh
@@ -50,6 +50,7 @@ echo "deb [arch=amd64,i386] http://security.ubuntu.com/ubuntu/ cosmic-security m
 echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu/ cosmic-updates main universe multiverse restricted" | tee -a /etc/apt/sources.list.d/cobbler.list;
 echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu/ cosmic-backports main universe multiverse restricted" | tee -a /etc/apt/sources.list.d/cobbler.list;
 
+echo "Checking ports list [${cobbler_architectures_ports_list}]...";
 if ["${cobbler_architectures_ports_list}" != ""]; then
   echo "Adding ports repositories...";
   echo "deb [arch=$cobbler_architectures_ports_list] http://ports.ubuntu.com/ubuntu-ports cosmic main universe multiverse restricted" | tee -a /etc/apt/sources.list.d/cobbler.list;
