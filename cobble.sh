dcdc84b5092cf027998f76d6845a2f24c800856c
Jay Rodgers <headmelted@gmail.com>
2018-09-10 11:56:04 +0100

    Adding linux-libc-dev

--- cobble.sh
+++ cobble.sh
@@ -20,7 +20,7 @@ for arch in $cobbler_foreign_architectures; do cobbler_packages_to_install="$cob
 libx11-dev:$arch libxdmcp-dev:$arch libdbus-1-3:$arch libpcre3:$arch libselinux1:$arch libp11-kit0:$arch libcomerr2:$arch libk5crypto3:$arch \
 libkrb5-3:$arch libpango-1.0-0:$arch libpangocairo-1.0-0:$arch libpangoft2-1.0-0:$arch libxcursor1:$arch libxfixes3:$arch libfreetype6:$arch libavahi-client3:$arch \
 libgssapi-krb5-2:$arch libtiff5:$arch fontconfig-config libgdk-pixbuf2.0-common libgdk-pixbuf2.0-0:$arch libfontconfig1:$arch libcups2:$arch \
-libcairo2:$arch libc6-dev:$arch libatk1.0-0:$arch libx11-xcb-dev:$arch libxtst6:$arch libxss-dev:$arch libxss1:$arch libgconf-2-4:$arch \
+libcairo2:$arch libc6-dev:$arch linux-libc-dev:$arch libatk1.0-0:$arch libx11-xcb-dev:$arch libxtst6:$arch libxss-dev:$arch libxss1:$arch libgconf-2-4:$arch \
 libasound2:$arch libnss3:$arch zlib1g:$arch"; done
 
 echo "Package install list: ${cobbler_packages_to_install}"
