94d6a34495c3f7bf22a28fa486633ee54dbb9ee7
Jay Rodgers <headmelted@gmail.com>
2018-09-13 10:01:48 +0100

    Update cook-all.sh

--- kitchen/steps/cook-all.sh
+++ kitchen/steps/cook-all.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# This script should be imported at the root level in order
+# to retain /kitchen as the current directory, which Cobbler
+# depends on to reconcile further steps in the build process.
+
 echo "Running all cook steps (get, build, package, test)";
-. ./cook.sh get build package test;
+. ./steps/cook.sh get build package test;
