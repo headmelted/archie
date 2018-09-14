7b2bb2fe333138565fc74cdd90c47ad832968688
Jay Rodgers <headmelted@gmail.com>
2018-09-14 15:24:54 +0100

    Create s390x.sh

---
+++ kitchen/env/linux/s390x.sh
@@ -0,0 +1,15 @@
+echo "Setting environment for s390x";
+LABEL="s390x_linux";
+CROSS_TOOLCHAIN="true";
+ARCH="s390x";
+RPM_ARCH="s390x";
+NPM_ARCH="";
+GNU_TRIPLET="s390x-linux-gnu";
+GNU_MULTILIB_TRIPLET="";
+CXX="s390x-linux-gnu-g++";
+CC="s390x-linux-gnu-gcc";
+PACKAGE_ARCH="s390x";
+QEMU_PACKAGE_ARCH="s390x";
+QEMU_ARCH="s390x";
+ELECTRON_ARCH="";
+VSCODE_ELECTRON_PLATFORM="";
