b13f3e24e8f67a58bd30ee4afc097d622c82341b
Jay Rodgers <headmelted@gmail.com>
2018-09-17 15:04:25 +0100

    Update cobble.sh

--- cobble.sh
+++ cobble.sh
@@ -136,6 +136,9 @@ mkdir "$COBBLER_CLEANROOM_DIRECTORY";
 echo "Creating [$COBBLER_ARCH] jail at [$COBBLER_CLEANROOM_DIRECTORY]";
 debootstrap --arch=$COBBLER_ARCH --variant=minbase stretch $COBBLER_CLEANROOM_DIRECTORY;
 
+echo "Echoing log";
+cat /kitchen/cleanroom/stretch/arm64/debootstrap/debootstrap.log;
+
 echo "Creating [$COBBLER_CLEANROOM_DIRECTORY/kitchen] for nested kitchen inside jail";
 mkdir "$COBBLER_CLEANROOM_DIRECTORY/kitchen"; 
 
