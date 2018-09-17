290297da196fe3a9292fd781d94409a0002f8cc7
Jay Rodgers <headmelted@gmail.com>
2018-09-17 09:11:46 +0100

    Update setup.sh

--- kitchen/env/linux/setup.sh
+++ kitchen/env/linux/setup.sh
@@ -7,6 +7,8 @@ export COBBLER_ROOT_DIRECTORY=/kitchen;
 export COBBLER_BUILDS_DIRECTORY=$COBBLER_ROOT_DIRECTORY/.builds/${COBBLER_ARCH};
 export COBBLER_CODE_DIRECTORY=$COBBLER_BUILDS_DIRECTORY/.code;
 export COBBLER_QEMU_TEST_METHOD=rootfs;
+export HOSTCXX='x86_64-linux-gnu-g++';
+export HOSTCC='x86_64-linux-gnu-gcc';
 
 echo "Setting Cobbler environment for [$COBBLER_ARCH]"
 . ./env/linux/$COBBLER_ARCH.sh
