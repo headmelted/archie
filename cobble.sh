ede53539c6c3c3073b051ba88f3920b7609ffc29
Jay Rodgers <headmelted@gmail.com>
2018-09-17 12:48:18 +0100

    Update cobble.sh

--- cobble.sh
+++ cobble.sh
@@ -129,6 +129,9 @@ cat /etc/apt/sources.list;
 echo "Installing QEMU packages, so that binfmt_misc is available during architecture-specific package installs for pre- and post- install hooks";
 apt-get install -y qemu qemu-user-static binfmt-support debootstrap fakeroot qemu-system-$qemu_package_architecture;
 
+echo "QEMU support installed for:";
+update-binfmts --display;
+
 echo "Installing standard and dependency packages"
 apt-get install -y curl gnupg git qemu qemu-user-static binfmt-support debootstrap fakeroot qemu-system-$qemu_package_architecture pkg-config libsecret-1-dev libglib2.0-dev software-properties-common xvfb wget python curl zip p7zip-full rpm graphicsmagick libwww-perl libxml-libxml-perl libxml-sax-expat-perl \
 dpkg-dev perl libconfig-inifiles-perl libxml-simple-perl liblocale-gettext-perl libdpkg-perl libconfig-auto-perl libdebian-dpkgcross-perl ucf debconf dpkg-cross tree \
