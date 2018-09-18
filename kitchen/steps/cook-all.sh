291ee23505045a8424376b03adaebb00f1075596
Jay Rodgers <headmelted@gmail.com>
2018-09-18 15:04:32 +0100

    Delete cook-all.sh

--- kitchen/steps/cook-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script should be imported at the root level in order
-# to retain /kitchen as the current directory, which Cobbler
-# depends on to reconcile further steps in the build process.
-
-echo "Running all cook steps (get, patch, build, package, test)";
-. ~/kitchen/steps/cook.sh get patch build test package; # Removed test for now until the hiccups are ironed out
