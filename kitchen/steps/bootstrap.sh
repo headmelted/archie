31a9274efdad9981f548b7b9841390c21877e23f
Jay Rodgers <headmelted@gmail.com>
2018-09-18 15:46:22 +0100

    Update bootstrap.sh

--- kitchen/steps/bootstrap.sh
+++ kitchen/steps/bootstrap.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 set -e;
+~/kitchen/env/linux/setup.sh;
+
+cat <<EOF
+
+ ██████╗ ██████╗ ██████╗ ██████╗ ██╗     ███████╗██████╗ 
+██╔════╝██╔═══██╗██╔══██╗██╔══██╗██║     ██╔════╝██╔══██╗
+██║     ██║   ██║██████╔╝██████╔╝██║     █████╗  ██████╔╝
+██║     ██║   ██║██╔══██╗██╔══██╗██║     ██╔══╝  ██╔══██╗
+╚██████╗╚██████╔╝██████╔╝██████╔╝███████╗███████╗██║  ██║
+ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝
+$COBBLER_OS_DISTRIBUTION_NAME:$COBBLER_OS_RELEASE_NAME                                     v0.0.1
+
+https://github.com/headmelted/cobbler
+https://hub.docker.com/r/headmelted/cobbler
+
+EOF
+
+~/kitchen/env/linux/display.sh;
+
+cat <<EOF
+------------ DEPENDENCY PACKAGE INSTALL LIST ------------
+${COBBLER_DEPENDENCY_PACKAGES}
+---------------------------------------------------------
+EOF
 
 ~/kitchen/steps/prepare_build_jail.sh "$COBBLER_CLEANROOM_DIRECTORY";
 
