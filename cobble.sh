319075b6c165c7b97b454d7f9ead19605cfa57b3
Jay Rodgers <headmelted@gmail.com>
2018-09-18 15:22:32 +0100

    Update cobble.sh

--- cobble.sh
+++ cobble.sh
@@ -4,6 +4,9 @@ set -e;
 echo "Entering kitchen to setup Cobbler";
 cd /root/kitchen;
 
+echo "Marking all files in kitchen executable (this directory should contain scripts and executables only!)";
+chmod +x /root/kitchen**/*;
+
 echo "Setting environment";
 . ./env/linux/setup.sh;
 
