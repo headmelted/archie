6af3afb123a1f1d054fdf77cc57881712ef5461f
Jay Rodgers <headmelted@gmail.com>
2018-09-18 13:58:06 +0100

    Update patch.sh

--- kitchen/steps/patch.sh
+++ kitchen/steps/patch.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -e;
+
+echo "Entering code directory";
+cd "$COBBLER_CODE_DIRECTORY";
     
 echo "Applying patches";
-for patch_script in ~/root/kitchen/cobbler/ingredients/patches/*.sh
+for patch_script in ~/kitchen/cobbler/ingredients/patches/*
 do
+
+  echo "Marking patch as executable [${patch_script}]";
+  chmod +x ~/kitchen/cobbler/ingredients/patches/${patch_script};
+  
   echo "Executing patch [${patch_script}]";
-  . ./${patch_script};
+  ~/kitchen/cobbler/ingredients/patches/${patch_script};
+  
 done
     
 echo "Applying overlays";
-/bin/cp --verbose -rf ~/root/kitchen/cobbler/ingredients/overlays/* ./;
+/bin/cp --verbose -rf ~/kitchen/cobbler/ingredients/overlays/* /;
