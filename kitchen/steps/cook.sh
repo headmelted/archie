6bd02450cd0cc3bee2d31b7da7a7e27807f1442d
Jay Rodgers <headmelted@gmail.com>
2018-09-18 15:07:17 +0100

    Update cook.sh

--- kitchen/steps/cook.sh
+++ kitchen/steps/cook.sh
@@ -14,11 +14,10 @@ else
 fi;
 
 echo "Starting to cook";
+echo "C compiler is ${CC}, C++ compiler is ${CXX}.";
 
-echo "C compiler is ${CC}, C++ compiler is ${CXX}."
-
-echo "Entering code directory [~/kitchen/build/code]";
-cd ~/kitchen/build/code;
+echo "Entering code directory [$COBBLER_CODE_DIRECTORY]";
+cd "$COBBLER_CODE_DIRECTORY";
 
 . ~/kitchen/cobbler/build.sh;
 
