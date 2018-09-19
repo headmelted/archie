098e0075cfc60072e056075887ea1d703e16c34b
Jay Rodgers <headmelted@gmail.com>
2018-09-19 16:57:56 +0100

    Update and rename prepare_build_jail.sh to build_target_jail_second_stage.sh

---
+++ kitchen/steps/build_target_jail_second_stage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e;
+
+echo "Setting environment";
+. ~/kitchen/env/linux/setup.sh;
+
+echo "Executing second stage debootstrap";
+/debootstrap/debootstrap --second-stage;
+
+echo "Updating [$COBBLER_ARCH] jail packages";
+apt-get update -yq;
+
+echo "Installing standard and dependency packages";
+apt-get install -y curl gnupg git pkg-config libsecret-1-dev libglib2.0-dev software-properties-common xvfb wget python curl zip p7zip-full rpm graphicsmagick libwww-perl libxml-libxml-perl libxml-sax-expat-perl dpkg-dev perl libconfig-inifiles-perl libxml-simple-perl liblocale-gettext-perl libdpkg-perl libconfig-auto-perl libdebian-dpkgcross-perl ucf debconf dpkg-cross tree libx11-dev libxkbfile-dev zlib1g-dev libc6-dev ${COBBLER_DEPENDENCY_PACKAGES};
+
+echo "Checking presence of NVM";
+. ~/kitchen/env/setup_nvm.sh;
+
+echo "Exiting chroot environment";
--- kitchen/steps/prepare_build_jail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e;
-
-# Call build_target_jail for the cleanroom
-# . /root/kitchen/steps/build_target_jail.sh "$COBBLER_CLEANROOM_DIRECTORY";
-
-. /root/kitchen/steps/jail.sh 'echo "Setting environment" && . /home/kitchen/env/linux/setup.sh && echo "Updating [$COBBLER_ARCH] jail packages" && apt-get update -yq && echo "Installing standard and dependency packages" && apt-get install -y curl gnupg git pkg-config libsecret-1-dev libglib2.0-dev software-properties-common xvfb wget python curl zip p7zip-full rpm graphicsmagick libwww-perl libxml-libxml-perl libxml-sax-expat-perl dpkg-dev perl libconfig-inifiles-perl libxml-simple-perl liblocale-gettext-perl libdpkg-perl libconfig-auto-perl libdebian-dpkgcross-perl ucf debconf dpkg-cross tree libx11-dev libxkbfile-dev zlib1g-dev libc6-dev ${COBBLER_DEPENDENCY_PACKAGES} && echo "Checking presence of NVM" && . /home/kitchen/env/setup_nvm.sh && echo "Exiting chroot environment"';
-
-echo "Jail is ready";
