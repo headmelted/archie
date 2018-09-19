36765f070e8db3b3c62260e260e3912cc118f46b
Jay Rodgers <headmelted@gmail.com>
2018-09-19 20:57:40 +0100

    Update stage.sh

--- kitchen/steps/stage.sh
+++ kitchen/steps/stage.sh
@@ -10,24 +10,42 @@ mkdir "$COBBLER_CLEANROOM_RELEASE_DIRECTORY";
 echo "Creating [$COBBLER_CLEANROOM_DIRECTORY]";
 mkdir "$COBBLER_CLEANROOM_DIRECTORY";
 
-if [ $COBBLER_ARCH == "amd64" || $COBBLER_STRATEGY != "emulate" ]; then
+if [ $COBBLER_ARCH == "amd64" || $COBBLER_STRATEGY == "cross" ]; then
 
   echo "QEMU not required, preparing environment";
   . /root/kitchen/steps/prepare.sh;
 
 else
+
+  if [ $COBBLER_STRATEGY == "emulate" ]; then
+
+    echo "Updating package sources"
+    apt-get update -yq;
+
+    echo "Installing apt-utils in isolation";
+    apt-get install -y apt-utils;
+
+    echo "Installing base Cobbler dependencies";
+    apt-get install -y qemu qemu-user-static debootstrap proot;
  
-  echo "Using debootstrap --foreign to create rootfs for jail"
-  debootstrap --foreign --verbose --arch=$COBBLER_ARCH --variant=minbase $COBBLER_OS_RELEASE_NAME $COBBLER_CLEANROOM_DIRECTORY;
+    echo "Using debootstrap --foreign to create rootfs for jail"
+    debootstrap --foreign --verbose --arch=$COBBLER_ARCH --variant=minbase $COBBLER_OS_RELEASE_NAME $COBBLER_CLEANROOM_DIRECTORY;
 
-  if [ "$COBBLER_ARCH" != "amd64" ]; then
+    if [ "$COBBLER_ARCH" != "amd64" ]; then
 
-    echo "Copying static QEMU for [$COBBLER_ARCH] into jail";
-    cp /usr/bin/qemu-$COBBLER_QEMU_ARCH-static $COBBLER_CLEANROOM_DIRECTORY/usr/bin/;
+      echo "Copying static QEMU for [$COBBLER_ARCH] into jail";
+      cp /usr/bin/qemu-$COBBLER_QEMU_ARCH-static $COBBLER_CLEANROOM_DIRECTORY/usr/bin/;
 
-    echo "Entering jail to complete setup";
-    . /root/kitchen/steps/jail.sh /home/kitchen/steps/prepare.sh;
+      echo "Entering jail to complete setup";
+      . /root/kitchen/steps/jail.sh /home/kitchen/steps/prepare.sh;
 
+    fi;
+  
+  else
+  
+    echo "ONLY cross AND emulate STRATEGIES ARE CURRENTLY SUPPORTED.";
+    exit;
+  
   fi;
   
 fi;
