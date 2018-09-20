0cac8319ae21767bee3b9cd08279c3a8d93efaec
Jay Rodgers <headmelted@gmail.com>
2018-09-20 08:54:34 +0100

    Update cobble.sh

--- cobble.sh
+++ cobble.sh
@@ -11,26 +11,7 @@ echo "Setting environment";
 . ./env/linux/setup.sh;
 . ./env/linux/display.sh;
 
-if [ "$COBBLER_ARCH" != "amd64" ]
-then
-  cobbler_cross_architectures="$COBBLER_ARCH";
-  if [ "$COBBLER_ARCH" != "i386" ]
-  then
-    cobbler_foreign_architectures="$COBBLER_ARCH";
-  fi
-fi
-
-cobbler_packages_to_install="gcc-$COBBLER_GNU_TRIPLET g++-$COBBLER_GNU_TRIPLET"
-
-for cobbler_cross_architecture in $cobbler_cross_architectures; do cobbler_packages_to_install="$cobbler_packages_to_install \
-crossbuild-essential-$cobbler_cross_architecture"; done
-
+echo "Staging environment";
 . ./steps/stage.sh;
-
-#echo "Adding yarn signing key"
-#curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-
-#echo "Adding yarn repository"
-#echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 echo "Cobbler ready for [$COBBLER_ARCH]";
