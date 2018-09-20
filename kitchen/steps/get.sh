94a81a2eeb2c46ae0f2b519865100460f029028d
Jay Rodgers <headmelted@gmail.com>
2018-09-20 07:27:08 +0100

    Update get.sh

--- kitchen/steps/get.sh
+++ kitchen/steps/get.sh
@@ -6,12 +6,6 @@ set -e;
 # Clone $COBBLER_GIT_ENDPOINT into the code folder, and make the executing
 # user the owner of the folder.
 
-echo "Remove .code folder if it exists"
-rm -rf $COBBLER_CODE_DIRECTORY;
-
-echo "Creating .code folder";
-mkdir $COBBLER_CODE_DIRECTORY;
-
 echo "Retrieving code from git endpoint [$COBBLER_GIT_ENDPOINT] into [$COBBLER_CODE_DIRECTORY]";
 git clone $COBBLER_GIT_ENDPOINT $COBBLER_CODE_DIRECTORY;
   
