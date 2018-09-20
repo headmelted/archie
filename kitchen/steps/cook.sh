d37b4ada90c3e27bc6af3c4b374f24a2e2a89060
Jay Rodgers <headmelted@gmail.com>
2018-09-20 12:15:30 +0100

    Update cook.sh

--- kitchen/steps/cook.sh
+++ kitchen/steps/cook.sh
@@ -8,7 +8,7 @@ echo "Checking for $COBBLER_GIT_ENDPOINT";
 if [ "$COBBLER_GIT_ENDPOINT" != "" ]; then
   echo "Cobbler is pointed at a git endpoint";
   . ~/kitchen/steps/get.sh;
-  . ~/kitchen/steps/patch.sh;
+  # . ~/kitchen/steps/patch.sh; # Removing patching (it should really be done in the context of the project's build script).
 else
   echo "Cobbler is not pointed at a git endpoint, assuming the current project is the one to build";
 fi;
