1421b6ea833b0ebc8777c5b611152a595a9c6208
Jay Rodgers <headmelted@gmail.com>
2018-09-21 17:18:54 +0100

    Create i386.sh

---
+++ kitchen/env/linux/i386.sh
@@ -0,0 +1,13 @@
+export COBBLER_LABEL='i386_linux';
+export COBBLER_CROSS_TOOLCHAIN='true';
+export COBBLER_ARCH='i386';
+export COBBLER_RPM_ARCH='x86';
+export COBBLER_GNU_TRIPLET='i686-linux-gnu';
+export COBBLER_GNU_MULTILIB_TRIPLET='';
+export COBBLER_PACKAGE_ARCH='i386';
+export COBBLER_QEMU_PACKAGE_ARCH='x86';
+export COBBLER_QEMU_ARCH='i386';
+export COBBLER_ELECTRON_ARCH='ia32';
+
+export VSCODE_ELECTRON_PLATFORM='ia32';
+export npm_config_target_arch='ia32';
