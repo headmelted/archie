b81d4763ee76fd04505c60498f3dd63d8e5b772c
Jay Rodgers <headmelted@gmail.com>
2018-09-23 16:30:16 +0100

    Update prepare.sh

--- kitchen/steps/prepare.sh
+++ kitchen/steps/prepare.sh
@@ -4,26 +4,26 @@ set -e;
 echo "Setting environment";
 . ~/kitchen/env/linux/setup.sh;
 
-if [ "$COBBLER_STRATEGY" -eq "emulate" ]; then
+if [ "$COBBLER_STRATEGY" == "emulate" ] ; then
 
   echo "We're in an emulated chroot, executing second stage debootstrap";
   /debootstrap/debootstrap --second-stage;
   
 else
 
-  if [ "$COBBLER_STRATEGY" -eq "hybrid" ]; then
+  if [ "$COBBLER_STRATEGY" == "hybrid" ] ; then
     echo "Using hybrid strategy, attempt to enter jail to execute second stage debootstrap";
     ~/kitchen/steps/jail.sh /debootstrap/debootstrap --second-stage;
   fi;
 
 fi;
 
-if [ "$COBBLER_STRATEGY" == "cross" ] || [ "$COBBLER_STRATEGY" == "hybrid" ]; then
+if [ "$COBBLER_STRATEGY" == "cross" ] || [ "$COBBLER_STRATEGY" == "hybrid" ] ; then
   
   echo "Adding cross-compilation target of [$COBBLER_ARCH]";
   dpkg --add-architecture $COBBLER_ARCH;
  
-  if [ "$COBBLER_ARCH" != "amd64" ] && [ "$COBBLER_ARCH" != "i386" ]; then packages_to_install="crossbuild-essential-$COBBLER_ARCH"; fi;
+  if [ "$COBBLER_ARCH" != "amd64" ] && [ "$COBBLER_ARCH" != "i386" ] ; then packages_to_install="crossbuild-essential-$COBBLER_ARCH"; fi;
   
 fi;
 
@@ -34,7 +34,7 @@ echo "Preparing to install dependencies";
 packages_to_install="$packages_to_install $COBBLER_HOST_DEPENDENCIES";
 
 for cobbler_dependency_package in $COBBLER_TARGET_DEPENDENCIES; do
-  if [ "$COBBLER_STRATEGY" == "cross" ] || [ "$COBBLER_STRATEGY" == "hybrid" ]; then
+  if [ "$COBBLER_STRATEGY" == "cross" ] || [ "$COBBLER_STRATEGY" == "hybrid" ] ; then
     cobbler_dependency_package="$cobbler_dependency_package:$COBBLER_ARCH";
   fi;
   packages_to_install="$packages_to_install $cobbler_dependency_package";
