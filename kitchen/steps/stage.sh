99d3beceee39c8214bde9486c5cae853bfd9c438
Jay Rodgers <headmelted@gmail.com>
2018-09-23 08:36:29 +0100

    Update stage.sh

--- kitchen/steps/stage.sh
+++ kitchen/steps/stage.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e;
 
-if [ $COBBLER_ARCH == "amd64" ] || [ $COBBLER_STRATEGY == "cross" ]; then
-
-  echo "QEMU not required, preparing environment";
-  . ~/kitchen/steps/prepare.sh;
-
-else
-
-  if [ $COBBLER_STRATEGY == "emulate" ]; then
+  if [ $COBBLER_STRATEGY == "hybrid"] || [ $COBBLER_STRATEGY == "emulate" ]; then
   
     echo "Creating [$COBBLER_CLEANROOM_ROOT_DIRECTORY]";
     mkdir "$COBBLER_CLEANROOM_ROOT_DIRECTORY";
@@ -26,7 +19,7 @@ else
     apt-get install -y apt-utils;
 
     echo "Installing base Cobbler dependencies";
-    apt-get install -y qemu qemu-user-static debootstrap proot;
+    apt-get install -y qemu qemu-user-static debootstrap;
  
     echo "Using debootstrap --foreign to create rootfs for [$COBBLER_ARCH] jail"
     debootstrap --foreign --verbose --arch=$COBBLER_ARCH --variant=minbase $COBBLER_OS_RELEASE_NAME $COBBLER_CLEANROOM_DIRECTORY;
@@ -36,15 +29,19 @@ else
 
     echo "Copying static QEMU for [$COBBLER_QEMU_ARCH] into [$COBBLER_ARCH] jail";
     cp /usr/bin/qemu-$COBBLER_QEMU_ARCH-static $COBBLER_CLEANROOM_DIRECTORY/usr/bin/;
-
-    echo "Entering [$COBBLER_ARCH] jail to complete setup";
-    . ~/kitchen/steps/jail.sh /home/kitchen/steps/prepare.sh;
-  
-  else
-  
-    echo "ONLY cross AND emulate STRATEGIES ARE CURRENTLY SUPPORTED.";
-    exit;
   
   fi;
   
+if [ $COBBLER_STRATEGY == "emulate"]; then
+
+    echo "Entering [$COBBLER_ARCH] jail to complete setup";
+    . ~/kitchen/steps/jail.sh /home/kitchen/steps/prepare.sh;
+    
+else
+
+  # For hybrid strategy, QEMU actually *will* be required, but not at this stage.
+  # (Only package installation is performed inside QEMU, compilation happens outside of it).
+  echo "QEMU not required, preparing environment";
+  . ~/kitchen/steps/prepare.sh;
+
 fi;
