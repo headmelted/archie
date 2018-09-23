2aaab10e83e5c3cf733f89b1db24a9925184efd8
Jay Rodgers <headmelted@gmail.com>
2018-09-23 13:05:30 +0100

    Update prepare.sh

--- kitchen/steps/prepare.sh
+++ kitchen/steps/prepare.sh
@@ -4,14 +4,14 @@ set -e;
 echo "Setting environment";
 . ~/kitchen/env/linux/setup.sh;
 
-if [ "$COBBLER_STRATEGY" == "emulate" ]; then
+if [ "$COBBLER_STRATEGY" -eq "emulate" ]; then
 
   echo "We're in an emulated chroot, executing second stage debootstrap";
   /debootstrap/debootstrap --second-stage;
   
 else
 
-  if [ "$COBBLER_STRATEGY" == "hybrid" ]; then
+  if [ "$COBBLER_STRATEGY" -eq "hybrid" ]; then
     echo "Using hybrid strategy, attempt to enter jail to execute second stage debootstrap";
     ~/kitchen/steps/jail.sh /debootstrap/debootstrap --second-stage;
   fi;
