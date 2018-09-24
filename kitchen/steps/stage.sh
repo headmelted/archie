5143d3f35537eb635a46956daddb65f59fbc7b53
Jay Rodgers <headmelted@gmail.com>
2018-09-24 15:42:13 +0100

    Update stage.sh

--- kitchen/steps/stage.sh
+++ kitchen/steps/stage.sh
@@ -29,11 +29,17 @@ echo "Staging for [$COBBLER_STRATEGY]";
     echo "Copying static QEMU for [$COBBLER_QEMU_ARCH] into [$COBBLER_ARCH] jail";
     cp /usr/bin/qemu-$COBBLER_QEMU_ARCH-static $COBBLER_CLEANROOM_DIRECTORY/usr/bin/;
     
-    echo "Manually installing qemu-$COBBLER_QEMU_ARCH binfmt";
-    mv ~/kitchen/qemu-binfmts/$COBBLER_QEMU_ARCH /usr/share/binfmts/qemu-$COBBLER_QEMU_ARCH;
+    echo "Displaying binfmts";
+    update-binfmts --display;
     
-    echo "Installing binfmt-support";
-    update-binfmts --import;
+    echo "Enabling binfmts";
+    update-binfmts --enable qemu-$COBBLER_QEMU_ARCH-static;
+    
+    #echo "Manually installing qemu-$COBBLER_QEMU_ARCH binfmt";
+    #mv ~/kitchen/qemu-binfmts/$COBBLER_QEMU_ARCH /usr/share/binfmts/qemu-$COBBLER_QEMU_ARCH;
+    
+    #echo "Installing binfmt-support";
+    #update-binfmts --import;
 
     echo "Creating kitchen directory inside cleanroom user /home";
     mkdir $COBBLER_CLEANROOM_DIRECTORY/home/kitchen;
