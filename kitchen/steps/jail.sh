7900e63c3fcd458fb26c6dd40323dc568183d4fc
Jay Rodgers <headmelted@gmail.com>
2018-09-24 16:21:18 +0100

    Update jail.sh

--- kitchen/steps/jail.sh
+++ kitchen/steps/jail.sh
@@ -5,4 +5,4 @@ set -e;
 #proot -b /root/kitchen:/home/kitchen -R $COBBLER_CLEANROOM_DIRECTORY -q qemu-$COBBLER_QEMU_ARCH-static "$@";
 
 echo "Entering [$COBBLER_ARCH] cleanroom ($COBBLER_CLEANROOM_DIRECTORY) via (fakeroot) to execute command";
-fakeroot fakechroot chroot $COBBLER_CLEANROOM_DIRECTORY "$@";
+fakeroot fakechroot chroot $COBBLER_CLEANROOM_DIRECTORY /usr/bin/qemu-$COBBLER_QEMU_ARCH-static /bin/bash -c "$@";
