eee2f445212da8cb9bb95c644c76275254a145da
Jay Rodgers <headmelted@gmail.com>
2018-09-24 12:06:24 +0100

    Update stage.sh

--- kitchen/steps/stage.sh
+++ kitchen/steps/stage.sh
@@ -29,8 +29,8 @@ echo "Staging for [$COBBLER_STRATEGY]";
     echo "Copying static QEMU for [$COBBLER_QEMU_ARCH] into [$COBBLER_ARCH] jail";
     cp /usr/bin/qemu-$COBBLER_QEMU_ARCH-static $COBBLER_CLEANROOM_DIRECTORY/usr/bin/;
     
-    echo "Attempting to setup binfmt support inside jail";
-    fakechroot fakeroot chroot /tmp/sid apt-get install -q hello
+    echo "Registering static QEMU in binfmts inside jail";
+    echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' > $COBBLER_CLEANROOM_DIRECTORY/proc/sys/fs/binfmt_misc/register;
     
     echo "Creating kitchen directory inside cleanroom user /home";
     mkdir $COBBLER_CLEANROOM_DIRECTORY/home/kitchen;
