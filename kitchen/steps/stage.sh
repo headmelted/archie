e7d3f5cc3dc92d45d425aa7cb9fc3ae8af1892fc
Jay Rodgers <headmelted@gmail.com>
2018-09-24 11:49:17 +0100

    Update stage.sh

--- kitchen/steps/stage.sh
+++ kitchen/steps/stage.sh
@@ -12,7 +12,7 @@ echo "Staging for [$COBBLER_STRATEGY]";
     apt-get install -y apt-utils;
 
     echo "Installing base Cobbler dependencies";
-    apt-get install -y qemu qemu-user-static debootstrap;
+    apt-get install -y qemu qemu-user-static debootstrap fakeroot;
     
     echo "Creating [$COBBLER_CLEANROOM_ROOT_DIRECTORY]";
     mkdir "$COBBLER_CLEANROOM_ROOT_DIRECTORY";
@@ -24,13 +24,13 @@ echo "Staging for [$COBBLER_STRATEGY]";
     mkdir "$COBBLER_CLEANROOM_DIRECTORY";
     
     echo "Using debootstrap --foreign to create rootfs for [$COBBLER_ARCH] jail"
-    debootstrap --foreign --verbose --arch=$COBBLER_ARCH --variant=minbase $COBBLER_OS_RELEASE_NAME $COBBLER_CLEANROOM_DIRECTORY;
-
-    echo "Creating kitchen directory inside cleanroom user /home";
-    mkdir $COBBLER_CLEANROOM_DIRECTORY/home/kitchen;
+    fakeroot debootstrap --foreign --verbose --arch=$COBBLER_ARCH --variant=minbase $COBBLER_OS_RELEASE_NAME $COBBLER_CLEANROOM_DIRECTORY;
 
     echo "Copying static QEMU for [$COBBLER_QEMU_ARCH] into [$COBBLER_ARCH] jail";
     cp /usr/bin/qemu-$COBBLER_QEMU_ARCH-static $COBBLER_CLEANROOM_DIRECTORY/usr/bin/;
+    
+    echo "Creating kitchen directory inside cleanroom user /home";
+    mkdir $COBBLER_CLEANROOM_DIRECTORY/home/kitchen;
   
   fi;
   
