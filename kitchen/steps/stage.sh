65e2feac97e08bd7be504282d7e3d49fa00791e0
Jay Rodgers <headmelted@gmail.com>
2018-09-25 10:50:42 +0100

    Update stage.sh

--- kitchen/steps/stage.sh
+++ kitchen/steps/stage.sh
@@ -48,17 +48,3 @@ echo "Staging for [$COBBLER_STRATEGY]";
     mkdir $COBBLER_CLEANROOM_DIRECTORY/home/kitchen;
   
   fi;
-  
-if [ "$COBBLER_STRATEGY" == "emulate" ] ; then
-
-    echo "Entering [$COBBLER_ARCH] jail to complete setup";
-    . ~/kitchen/steps/jail.sh /home/kitchen/steps/prepare.sh;
-    
-else
-
-  # For hybrid strategy, QEMU actually *will* be required, but not at this stage.
-  # (Only package installation is performed inside QEMU, compilation happens outside of it).
-  echo "QEMU not required, preparing environment";
-  . ~/kitchen/steps/prepare.sh;
-
-fi;
