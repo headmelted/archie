b8a4d64f8b436aa23bf52c5906a5f307658660b1
Jay Rodgers <headmelted@gmail.com>
2018-09-25 10:51:24 +0100

    Create bootstrap_prepare.sh

---
+++ kitchen/steps/bootstrap_prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e;
+  
+if [ "$COBBLER_STRATEGY" == "emulate" ] ; then
+
+    echo "Entering [$COBBLER_ARCH] jail to complete setup";
+    . ~/kitchen/steps/jail.sh /home/kitchen/steps/prepare.sh;
+    
+else
+
+  # For hybrid strategy, QEMU actually *will* be required, but not at this stage.
+  # (Only package installation is performed inside QEMU, compilation happens outside of it).
+  echo "QEMU not required, preparing environment";
+  . ~/kitchen/steps/prepare.sh;
+
+fi;
