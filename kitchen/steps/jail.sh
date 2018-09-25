a293b1359c71382e4857f654ac6a61fc48dd8aa6
Jay Rodgers <headmelted@gmail.com>
2018-09-25 09:34:21 +0100

    Update jail.sh

--- kitchen/steps/jail.sh
+++ kitchen/steps/jail.sh
@@ -8,11 +8,11 @@ echo "Chroot is at: $(which chroot) lets see";
 echo "Testing fakechroot at ($COBBLER_CLEANROOM_DIRECTORY)";
 fakechroot fakeroot chroot $COBBLER_CLEANROOM_DIRECTORY /usr/bin/qemu-$COBBLER_QEMU_ARCH-static -L $COBBLER_CLEANROOM_DIRECTORY pwd;
 
-echo "Entering [$COBBLER_ARCH] cleanroom ($COBBLER_CLEANROOM_DIRECTORY) to debug fakeroot";
-fakechroot fakeroot chroot $COBBLER_CLEANROOM_DIRECTORY /usr/bin/qemu-$COBBLER_QEMU_ARCH-static -L $COBBLER_CLEANROOM_DIRECTORY -D ~/kitchen/qemu.log readelf -d /bin/bash
+#echo "Entering [$COBBLER_ARCH] cleanroom ($COBBLER_CLEANROOM_DIRECTORY) to debug fakeroot";
+#fakechroot fakeroot chroot $COBBLER_CLEANROOM_DIRECTORY /usr/bin/qemu-$COBBLER_QEMU_ARCH-static -L $COBBLER_CLEANROOM_DIRECTORY -D ~/kitchen/qemu.log readelf -d /bin/bash
 
-echo "Entering [$COBBLER_ARCH] cleanroom ($COBBLER_CLEANROOM_DIRECTORY) via (fakeroot) to execute command";
-fakechroot fakeroot /usr/sbin/chroot $COBBLER_CLEANROOM_DIRECTORY /usr/bin/qemu-$COBBLER_QEMU_ARCH-static -L $COBBLER_CLEANROOM_DIRECTORY -D ~/kitchen/qemu.log /bin/bash -c "$@";
+#echo "Entering [$COBBLER_ARCH] cleanroom ($COBBLER_CLEANROOM_DIRECTORY) via (fakeroot) to execute command";
+#fakechroot fakeroot /usr/sbin/chroot $COBBLER_CLEANROOM_DIRECTORY /usr/bin/qemu-$COBBLER_QEMU_ARCH-static -L $COBBLER_CLEANROOM_DIRECTORY -D ~/kitchen/qemu.log /bin/bash -c "$@";
 
 echo "QEMU log:"
 cat ~/kitchen/qemu.log
