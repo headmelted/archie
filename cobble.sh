f9516be79909ddeb20e98c55036e9c85bb988ca9
Jay Rodgers <headmelted@gmail.com>
2018-09-27 15:54:30 +0100

    Update cobble.sh

--- cobble.sh
+++ cobble.sh
@@ -6,14 +6,14 @@ export COBBLER_HOME=$HOME;
 
 echo "COBBLER_HOME is $COBBLER_HOME";
 
-echo "Entering kitchen to setup Cobbler";
-cd ~/kitchen;
+#echo "Entering kitchen to setup Cobbler";
+#cd ~/kitchen;
 
 echo "Setting environment";
 . ./env/linux/setup.sh;
 . ./env/linux/display.sh;
 
-echo "Staging environment";
-. ./steps/stage.sh;
+#echo "Staging environment";
+#. ./steps/stage.sh;
 
 echo "Cobbler ready for [$COBBLER_ARCH]";
