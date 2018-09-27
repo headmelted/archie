5c225da7f7e8d693cc93b7ed4d821315373bf13e
Jay Rodgers <headmelted@gmail.com>
2018-09-27 13:57:36 +0100

    Update build_rootfs.sh

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -7,17 +7,11 @@ COBBLER_HOME=$(pwd);
 echo "Setting Cobbler environment";
 . ./kitchen/env/linux/setup.sh;
 
-echo "Installing QEMU dependencies";
-. ./kitchen/steps/install_qemu_dependencies.sh;
-
 echo "Installing debootstrap";
 apt-get install -y debootstrap;
 
 echo "Using debootstrap --foreign to create rootfs for [$COBBLER_ARCH] jail"
 fakeroot debootstrap --foreign --verbose --arch=$COBBLER_ARCH --variant=minbase stretch rootfs;
-
-echo "Checking binfmts";
-update-binfmts --display;
 
 echo "Injecting APT sources list";
 mv sources.list rootfs/etc/apt/;
@@ -25,26 +19,26 @@ mv sources.list rootfs/etc/apt/;
 echo "Reading rootfs sources list";
 cat rootfs/etc/apt/sources.list;
 
-#echo "Copying static QEMU (using the Resin.IO patched version - ToDo: ADD REFERENCE IN README) for [$COBBLER_QEMU_ARCH] into [$COBBLER_ARCH] jail";
-#cp ./kitchen/qemu-$COBBLER_QEMU_ARCH-static rootfs/usr/bin/;
+echo "Override QEMU interception mode on build host to construct rootfs";
+cobbler_qemu_interception_mode_original=$COBBLER_QEMU_INTERCEPTION_MODE;
+COBBLER_QEMU_INTERCEPTION_MODE="binfmt_misc" # Azure supports binfmt_misc, so override here and then set back to whatever was specified.
+
+echo "Installing QEMU dependencies";
+. ./kitchen/steps/install_qemu_dependencies.sh;
+
+echo "Restoring QEMU interception mode to [$cobbler_qemu_interception_mode_original]";
+export COBBLER_QEMU_INTERCEPTION_MODE=$cobbler_qemu_interception_mode_original;
+
+if [ ${COBBLER_QEMU_INTERCEPTION_MODE} == "binfmt_misc" ] ; then
+  echo "Displaying binfmt bindings";
+  update-binfmts --display;
+fi;
+
+echo "Copying static QEMU (using the Resin.IO patched version - ToDo: ADD REFERENCE IN README) for [$COBBLER_QEMU_ARCH] into [$COBBLER_ARCH] jail";
+cp ./kitchen/qemu-$COBBLER_QEMU_ARCH-static rootfs/usr/bin/;
     
-#echo "Marking static [rootfs/usr/bin/qemu-$COBBLER_QEMU_ARCH-static] as executable";
-#chmod +x rootfs/usr/bin/qemu-$COBBLER_QEMU_ARCH-static;
-
-#qwrap_path=rootfs/usr/bin/qwrap_${COBBLER_QEMU_ARCH}.sh
-
-#echo "Writing QEMU bash wrapper to ${qwrap_path}";
-#cat > ${qwrap_path} << 'endmsg'
-##!/bin/bash
-#echo "Executing command using QEMU shell";
-#"$@"
-#endmsg
-
-#echo "Marking ${qwrap_path} as executable";
-#chmod +x ${qwrap_path};
-
-#echo "Contents of ${qwrap_path}";
-#cat ${qwrap_path};
+echo "Marking static [rootfs/usr/bin/qemu-$COBBLER_QEMU_ARCH-static] as executable";
+chmod +x rootfs/usr/bin/qemu-$COBBLER_QEMU_ARCH-static;
 
 #echo "Entering [$COBBLER_ARCH] cleanroom (proot) to execute second stage of debootstrap";
 #sudo proot -b $COBBLER_HOME/kitchen:/kitchen -q qemu-$COBBLER_QEMU_ARCH-static -R rootfs uname -a && sudo dpkg --configure -a && sudo apt-get update -yq;
