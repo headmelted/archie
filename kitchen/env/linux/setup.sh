171e35afebb95525dc2385f88b073479e39c24c6
Jay Rodgers <headmelted@gmail.com>
2018-09-27 14:01:11 +0100

    Update setup.sh

--- kitchen/env/linux/setup.sh
+++ kitchen/env/linux/setup.sh
@@ -14,20 +14,22 @@ export COBBLER_OS_RELEASE_NAME=stretch;
 echo "Setting Cobbler environment for [$COBBLER_ARCH]"
 . $COBBLER_HOME/kitchen/env/linux/$COBBLER_ARCH.sh;
 
-echo "Checking kernel for binfmt_misc support";
-config_binfmt_misc=$(zcat /proc/config.gz | grep -i binfmt_misc);
-if [ "$config_binfmt_misc" == "CONFIG_BINFMT_MISC=y" ] ; then
-  export COBBLER_QEMU_INTERCEPTION_MODE="binfmt_misc";
-else
-  echo "ToDo add support for CAP_SYS_PTRACE and proot as a fallback, and then potentially QEMU EXECVE() interception as a last resort."
-  exit;
-  
-  echo "Disabling SECCOMP for proot";
-  export PROOT_NO_SECCOMP=1;
-  
-  echo "Setting QEMU_EXECVE flag to allow QEMU to intercept execve() calls without binfmt_misc";
-  export QEMU_EXECVE=1;
-fi;
+#echo "Checking kernel for binfmt_misc support";
+#config_binfmt_misc=$(zcat /proc/config.gz | grep -i binfmt_misc);
+#if [ "$config_binfmt_misc" == "CONFIG_BINFMT_MISC=y" ] ; then
+#  export COBBLER_QEMU_INTERCEPTION_MODE="binfmt_misc";
+#else
+#  echo "ToDo add support for CAP_SYS_PTRACE and proot as a fallback, and then potentially QEMU EXECVE() interception as a last resort."
+#  exit;
+#  
+#  echo "Disabling SECCOMP for proot";
+#  export PROOT_NO_SECCOMP=1;
+#  
+#  echo "Setting QEMU_EXECVE flag to allow QEMU to intercept execve() calls without binfmt_misc";
+#  export QEMU_EXECVE=1;
+#fi;
+
+export COBBLER_QEMU_INTERCEPTION_MODE="binfmt_misc";
 
 echo "Setting cleanroom paths";
 export COBBLER_CLEANROOM_DIRECTORY=/root/jail;
