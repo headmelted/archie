de1d8562b4f18375db5c974f00707dae75cfee28
Jay Rodgers <headmelted@gmail.com>
2018-09-27 13:41:20 +0100

    Create install_dependencies.sh

---
+++ kitchen/steps/install_dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e;
+
+if [ ${COBBLER_QEMU_INTERCEPTION_MODE} == "proot" ] ; then
+  cobbler_qemu_dependencies="proot";
+elif [ ${COBBLER_QEMU_INTERCEPTION_MODE} == "execve" ] ; then
+  cobbler_qemu_dependencies="fakechroot fakeroot";
+else
+  cobbler_qemu_dependencies=""; # binfmt_misc and virtualize
+fi;
+
+echo "Installing base Cobbler dependencies";
+apt-get install -y debootstrap qemu-user-static ${cobbler_qemu_dependencies};
