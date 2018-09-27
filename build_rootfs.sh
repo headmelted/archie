1b8fa08ad85beb15e46a8f124cca40516997e1e5
Jay Rodgers <headmelted@gmail.com>
2018-09-27 12:55:15 +0100

    Update build_rootfs.sh

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -20,29 +20,29 @@ mv sources.list rootfs/etc/apt/;
 echo "Reading rootfs sources list";
 cat rootfs/etc/apt/sources.list;
 
-echo "Copying static QEMU (using the Resin.IO patched version - ToDo: ADD REFERENCE IN README) for [$COBBLER_QEMU_ARCH] into [$COBBLER_ARCH] jail";
-cp ./kitchen/qemu-$COBBLER_QEMU_ARCH-static rootfs/usr/bin/;
+#echo "Copying static QEMU (using the Resin.IO patched version - ToDo: ADD REFERENCE IN README) for [$COBBLER_QEMU_ARCH] into [$COBBLER_ARCH] jail";
+#cp ./kitchen/qemu-$COBBLER_QEMU_ARCH-static rootfs/usr/bin/;
     
-echo "Marking static [rootfs/usr/bin/qemu-$COBBLER_QEMU_ARCH-static] as executable";
-chmod +x rootfs/usr/bin/qemu-$COBBLER_QEMU_ARCH-static;
+#echo "Marking static [rootfs/usr/bin/qemu-$COBBLER_QEMU_ARCH-static] as executable";
+#chmod +x rootfs/usr/bin/qemu-$COBBLER_QEMU_ARCH-static;
 
-qwrap_path=rootfs/usr/bin/qwrap_${COBBLER_QEMU_ARCH}.sh
+#qwrap_path=rootfs/usr/bin/qwrap_${COBBLER_QEMU_ARCH}.sh
 
-echo "Writing QEMU bash wrapper to ${qwrap_path}";
-cat > ${qwrap_path} << 'endmsg'
-#!/bin/bash
-echo "Executing command using QEMU shell";
-"$@"
-endmsg
+#echo "Writing QEMU bash wrapper to ${qwrap_path}";
+#cat > ${qwrap_path} << 'endmsg'
+##!/bin/bash
+#echo "Executing command using QEMU shell";
+#"$@"
+#endmsg
 
-echo "Marking ${qwrap_path} as executable";
-chmod +x ${qwrap_path};
+#echo "Marking ${qwrap_path} as executable";
+#chmod +x ${qwrap_path};
 
-echo "Contents of ${qwrap_path}";
-cat ${qwrap_path};
+#echo "Contents of ${qwrap_path}";
+#cat ${qwrap_path};
 
 #echo "Entering [$COBBLER_ARCH] cleanroom (proot) to execute second stage of debootstrap";
 #sudo proot -b $COBBLER_HOME/kitchen:/kitchen -q qemu-$COBBLER_QEMU_ARCH-static -R rootfs uname -a && sudo dpkg --configure -a && sudo apt-get update -yq;
 
 #sudo ./rootfs/usr/bin/qemu-arm-static -L rootfs uname -a
-fakechroot fakeroot chroot rootfs /usr/bin/qwrap_${COBBLER_QEMU_ARCH}.sh uname -a
+chroot rootfs uname -a
