9b7912a41b10cfd94973001122216fe82ed6f2c4
Jay Rodgers <headmelted@gmail.com>
2018-09-28 14:27:25 +0100

    Update build_rootfs.sh

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -12,8 +12,13 @@ sudo apt-get install -y debootstrap fakechroot fakeroot proot;
 echo "Using debootstrap --foreign to create rootfs for [$COBBLER_ARCH] jail"
 sudo debootstrap --foreign --verbose --arch=$COBBLER_ARCH --variant=minbase stretch rootfs;
 
-echo "Mounting proc into rootfs";
-sudo mount --bind /proc rootfs/proc;
+echo "Manually mounting proc, sys and dev into rootfs";
+cd rootfs;
+sudo mount --bind /dev dev/
+sudo mount --bind /sys sys/
+sudo mount --bind /proc proc/
+sudo mount --bind /dev/pts dev/pts
+cd ..;
 
 #echo "rootfs lib:";
 #ls rootfs/lib;
