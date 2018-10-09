f1d27b7b3116f9cc34d665827ce8dc2de20c66a9
Jay Rodgers <headmelted@gmail.com>
2018-10-09 08:05:15 +0000

    Changing around build process

--- kitchen/steps/bootstrap_prepare.sh
+++ kitchen/steps/bootstrap_prepare.sh
@@ -4,13 +4,13 @@ set -e;
 if [ "$COBBLER_STRATEGY" == "emulate" ] ; then
 
     echo "Entering [$COBBLER_ARCH] jail to complete setup";
-    . ~/kitchen/steps/jail.sh /home/kitchen/steps/prepare.sh;
+    . ~/kitchen/steps/jail.sh /home/kitchen/steps/install_dependencies.sh;
     
 else
 
   # For hybrid strategy, QEMU actually *will* be required, but not at this stage.
   # (Only package installation is performed inside QEMU, compilation happens outside of it).
   echo "QEMU not required, preparing environment";
-  . ~/kitchen/steps/prepare.sh;
+  . ~/kitchen/steps/install_dependencies.sh;
 
 fi;
