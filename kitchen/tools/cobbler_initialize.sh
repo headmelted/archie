9456c08ba442524802a7f8db49342f407514436c
Jay Rodgers <headmelted@gmail.com>
2018-10-10 10:41:52 +0000

    Restoring COBBLER_STRATEGY and COBBLER_ARCH environment variables

--- kitchen/tools/cobbler_initialize.sh
+++ kitchen/tools/cobbler_initialize.sh
@@ -3,14 +3,14 @@
 echo "Setting COBBLER_HOME";
 export COBBLER_HOME=$HOME;
 
-echo "Marking tools scripts executable";
-chmod +x ./kitchen/tools/*.sh;
+#echo "Marking tools scripts executable";
+#chmod +x $COBBLER_HOME/kitchen/tools/*.sh;
 
-echo "Setting PATH to include Cobbler tools"
-export PATH="$COBBLER_HOME/kitchen/tools/:$PATH"
+#echo "Setting PATH to include Cobbler tools"
+#export PATH="$COBBLER_HOME/kitchen/tools/:$PATH"
 
 echo "Initializing environment for [${COBBLER_DOCKER_TAG}]";
-. ./kitchen/env/linux/setup.sh;
+. $COBBLER_HOME/kitchen/env/setup.sh;
 
 if [ "${COBBLER_STRATEGY}" == "hybrid" ] || [ "${COBBLER_STRATEGY}" == "emulate" ]; then
   echo "Downloading rootfs from prebootstrap for [${COBBLER_STRATEGY}] image";
@@ -23,4 +23,4 @@ apt-get update -yq;
 echo "Ensuring git is installed";
 apt-get install -y git;
 
-. ~/kitchen/env/linux/display.sh;
+. $COBBLER_HOME/kitchen/env/linux/display.sh;
