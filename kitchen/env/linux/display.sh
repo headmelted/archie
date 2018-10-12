6a8f7697bd80267eda220a3617c768b011f41ca5
Jay Rodgers <headmelted@gmail.com>
2018-10-12 12:08:31 +0000

    Debugging github token

--- kitchen/env/linux/display.sh
+++ kitchen/env/linux/display.sh
@@ -14,20 +14,14 @@ echo "https://hub.docker.com/r/headmelted/cobbler"
 
 echo "------------------ ENVIRONMENT DETAILS ------------------"
 echo "Target architecture: $COBBLER_ARCH"
-echo "Non-native target architectures: $cobbler_foreign_architectures"
-echo "Cross-compile architectures: $cobbler_cross_architectures"
 echo "QEMU architectures: $COBBLER_QEMU_ARCH"
 echo "QEMU system emulator set: qemu-system-$COBBLER_QEMU_PACKAGE_ARCH"
-echo "C compilers (gcc-): $COBBLER_GNU_TRIPLET"
-echo "C++ compilers (gpp-): $COBBLER_GNU_TRIPLET"
-echo "---------------------------------------------------------"
-
-echo "CC (version) --------------"
+echo "CC (version) ---------------------------------------------------"
 eval "$CC -v";
-echo "CC (configuration) --------"
+echo "CC (configuration) ---------------------------------------------"
 echo $CC
-echo "CXX (version) -------------"
+echo "CXX (version) --------------------------------------------------"
 eval "$CXX -v";
-echo "CXX (configuration) -------"
+echo "CXX (configuration) --------------------------------------------"
 echo $CXX
-echo "---------------------------"
+echo "----------------------------------------------------------------"
