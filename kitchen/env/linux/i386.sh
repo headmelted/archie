1639b687fb9f78f6f03fe4a8f0af99651e871fdb
Jay Rodgers <headmelted@gmail.com>
2018-10-12 09:03:49 +0000

    Adding PKG_CONFIG_PATH

--- kitchen/env/linux/i386.sh
+++ kitchen/env/linux/i386.sh
@@ -7,7 +7,8 @@ export COBBLER_GNU_MULTILIB_TRIPLET='';
 export COBBLER_PACKAGE_ARCH='i386';
 export COBBLER_QEMU_PACKAGE_ARCH='x86';
 export COBBLER_QEMU_ARCH='i386';
-export COBBLER_ELECTRON_ARCH='ia32';
+export COBBLER_ELECTRON_ARCH='ia32'; 
+export PKG_CONFIG_PATH="/usr/lib/i386-linux-gnu/pkgconfig"
 
 export VSCODE_ELECTRON_PLATFORM='ia32';
 export npm_config_target_arch='ia32';
