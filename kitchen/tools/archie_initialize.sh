041d22cdec5d2f66f5d41401e4194ac663be2b99
Jay Rodgers <headmelted@gmail.com>
2018-10-16 21:52:13 +0000

    Adding wget for prebootstrap

--- kitchen/tools/archie_initialize.sh
+++ kitchen/tools/archie_initialize.sh
@@ -11,6 +11,8 @@ if [ "${ARCHIE_STRATEGY}" == "hybrid" ] || [ "${ARCHIE_STRATEGY}" == "emulate" ]
   apt-get update -yq;
   echo "Installing wget for prebootstrap";
   apt-get install -y wget;
+  echo "Creating /root/jail directory";
+  mkdir /root/jail;
   echo "Downloading rootfs from prebootstrap for [${ARCHIE_STRATEGY}] image";
   wget -c "https://github.com/headmelted/prebootstrap/releases/download/Oct-18/prebootstrap_stretch_minbase_${ARCHIE_ARCH}_rootfs.tar.gz" -O - | tar -xz -C /root/jail/
 fi
