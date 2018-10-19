b9a8ed7c4f166f7beddfcae879a7bac1d3494e3b
Jay Rodgers <headmelted@gmail.com>
2018-10-19 11:25:42 +0000

    Debugging chroot

--- kitchen/tools/archie_start_build.sh
+++ kitchen/tools/archie_start_build.sh
@@ -12,15 +12,15 @@ set -e;
 #  echo "Archie is not pointed at a git endpoint, assuming the current project is the one to build";
 #fi;
 
-build_command=". $ARCHIE_HOME/kitchen/tools/archie_install_dependencies.sh && . /root/build/build.sh"
-
 chmod +x $ARCHIE_HOME/kitchen/tools/*.sh;
 chmod +x /root/build/build.sh;
 
 if [ "$ARCHIE_STRATEGY" == "emulate" ]; then
   echo "Entering jail to start build in new bash shell";
-  . $ARCHIE_HOME/kitchen/tools/archie_jail.sh /bin/bash -C "$build_command";
+  . $ARCHIE_HOME/kitchen/tools/archie_jail.sh '$ARCHIE_HOME/kitchen/tools/archie_install_dependencies.sh';
+  . $ARCHIE_HOME/kitchen/tools/archie_jail.sh '/root/build/build.sh';
 else
   echo "Starting build in new bash shell [$build_command]";
-  /bin/bash -c "$build_command";
+  /bin/bash -c "$ARCHIE_HOME/kitchen/tools/archie_install_dependencies.sh"
+  /bin/bash -c "/root/build/build.sh";
 fi;
