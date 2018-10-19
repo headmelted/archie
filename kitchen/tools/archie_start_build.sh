56deab0ab565f2130e7ce3a849d518797d873985
Jay Rodgers <headmelted@gmail.com>
2018-10-19 11:28:48 +0000

    Debugging chroot

--- kitchen/tools/archie_start_build.sh
+++ kitchen/tools/archie_start_build.sh
@@ -13,11 +13,11 @@ set -e;
 #fi;
 
 chmod +x $ARCHIE_HOME/kitchen/tools/*.sh;
-chmod +x /root/build/build.sh;
+chmod +x $ARCHIE_HOME/build/build.sh;
 
 if [ "$ARCHIE_STRATEGY" == "emulate" ]; then
   echo "Entering jail to start build in new bash shell";
-  . $ARCHIE_HOME/kitchen/tools/archie_jail.sh '$ARCHIE_HOME/kitchen/tools/archie_install_dependencies.sh';
+  . $ARCHIE_HOME/kitchen/tools/archie_jail.sh '/root/kitchen/tools/archie_install_dependencies.sh';
   . $ARCHIE_HOME/kitchen/tools/archie_jail.sh '/root/build/build.sh';
 else
   echo "Starting build in new bash shell [$build_command]";
