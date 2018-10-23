70bc39a73a88592ed5073d7dd1bddc7917867bd3
Jay Rodgers <headmelted@gmail.com>
2018-10-23 13:09:47 +0100

    Create archie_build.sh

---
+++ kitchen/tools/archie_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e;
+
+echo "Installing dependencies";
+. /root/kitchen/tools/archie_install_dependencies.sh;
+
+echo "Setting up environment";
+. /root/kitchen/env/setup.sh;
+
+echo "Running build script";
+. /root/build/build.sh';
