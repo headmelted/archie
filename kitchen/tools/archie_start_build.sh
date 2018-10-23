1924f5015a2268bfcc4ef0f63883f379f2323ac6
Jay Rodgers <headmelted@gmail.com>
2018-10-23 10:56:45 +0100

    Update archie_start_build.sh

--- kitchen/tools/archie_start_build.sh
+++ kitchen/tools/archie_start_build.sh
@@ -18,9 +18,9 @@ chmod +x $ARCHIE_HOME/build/build.sh;
 if [ "$ARCHIE_STRATEGY" == "emulate" ]; then
   echo "Entering jail to start build in new bash shell";
   . $ARCHIE_HOME/kitchen/tools/archie_jail.sh '/root/kitchen/tools/archie_install_dependencies.sh';
-  . $ARCHIE_HOME/kitchen/tools/archie_jail.sh '/root/build/build.sh';
+  . $ARCHIE_HOME/kitchen/tools/archie_jail.sh '/root/kitchen/env/setup.sh && /root/build/build.sh';
 else
   echo "Starting build in new bash shell [$build_command]";
   /bin/bash -c "/root/kitchen/tools/archie_install_dependencies.sh"
-  /bin/bash -c "/root/build/build.sh";
+  /bin/bash -c "/root/kitchen/env/setup.sh && /root/build/build.sh";
 fi;
