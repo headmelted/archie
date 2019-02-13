60251f87ab913a3ad3ccd3fafc069f58463c8772
Jay Rodgers <headmelted@gmail.com>
2019-02-13 17:23:01 +0000

    Update archie_initialize_compilers.sh

--- kitchen/tools/archie_initialize_compilers.sh
+++ kitchen/tools/archie_initialize_compilers.sh
@@ -5,19 +5,19 @@ echo "Setting compiler configuration for [$ARCHIE_STRATEGY]";
 
 ARCHIE_CROSS_LIB_PATH="/usr/lib/$ARCHIE_GNU_TRIPLET";
 
-pkg_config_linkage_list="";
-for archie_dependency_package in $ARCHIE_TARGET_DEPENDENCIES; do
-  for pkg_config_file in $(dpkg-query -L $archie_dependency_package | grep .pc$); do
-    pkg_config_id=$(basename $pkg_config_file .pc);
-    pkg-config --libs --cflags $pkg_config_id
-  done;
-  pkg_config_linkage_list=$( pkg-config --libs --cflags );
-  pkg_config_linkage_list="$pkg_config_linkage_list ";
-done;
-  
-echo "PKG-CONFIG LINKAGE LIST (NOT CURRENTLY USED)--------------------------------------"
-echo pkg_config_linkage_list
-echo "----------------------------------------------------------------------------------"
+#pkg_config_linkage_list="";
+#for archie_dependency_package in $ARCHIE_TARGET_DEPENDENCIES; do
+#  for pkg_config_file in $(dpkg-query -L $archie_dependency_package | grep .pc$); do
+#    pkg_config_id=$(basename $pkg_config_file .pc);
+#    pkg-config --libs --cflags $pkg_config_id
+#  done;
+#  pkg_config_linkage_list=$( pkg-config --libs --cflags );
+#  pkg_config_linkage_list="$pkg_config_linkage_list ";
+#done;
+#  
+#echo "PKG-CONFIG LINKAGE LIST (NOT CURRENTLY USED)--------------------------------------"
+#echo pkg_config_linkage_list
+#echo "----------------------------------------------------------------------------------"
 
 linkage_list="-L$ARCHIE_CROSS_LIB_PATH -I/usr/include/$ARCHIE_GNU_TRIPLET";
 
