4aaa652a6509cb66a48c625409ac3fd1eb0e2b47
Jay Rodgers <headmelted@gmail.com>
2019-02-14 14:50:27 +0000

    Update ppc64el.sh

--- kitchen/env/linux/ppc64el.sh
+++ kitchen/env/linux/ppc64el.sh
@@ -3,11 +3,11 @@ export ARCHIE_CROSS_TOOLCHAIN="true";
 export ARCHIE_ARCH="ppc64el";
 export ARCHIE_RPM_ARCH="ppc64el";
 export ARCHIE_GNU_TRIPLET="powerpc64le-linux-gnu";
+export ARCHIE_HEADERS_GNU_TRIPLET="powerpc64le-linux-gnu";
 export ARCHIE_GNU_MULTILIB_TRIPLET="";
 export ARCHIE_PACKAGE_ARCH="ppc64el";
 export ARCHIE_QEMU_PACKAGE_ARCH="ppc";
 export ARCHIE_QEMU_ARCH="ppc64le";
 export ARCHIE_ELECTRON_ARCH="";
-export PKG_CONFIG_PATH="/usr/lib/powerpc64le-linux-gnu/pkgconfig";
 
 export VSCODE_ELECTRON_PLATFORM="";
