253bbae3f67fab7aae077ca2386116992d72f8b9
Jay Rodgers <headmelted@gmail.com>
2019-02-14 14:48:58 +0000

    Update amd64.sh

--- kitchen/env/linux/amd64.sh
+++ kitchen/env/linux/amd64.sh
@@ -3,6 +3,7 @@ export ARCHIE_CROSS_TOOLCHAIN='false';
 export ARCHIE_ARCH='amd64';
 export ARCHIE_RPM_ARCH='x86_64';
 export ARCHIE_GNU_TRIPLET='x86_64-linux-gnu';
+export ARCHIE_HEADERS_GNU_TRIPLET='x86_64-linux-gnu';
 export ARCHIE_GNU_MULTILIB_TRIPLET='';
 export ARCHIE_PACKAGE_ARCH='x64';
 export ARCHIE_QEMU_PACKAGE_ARCH='amd64';
