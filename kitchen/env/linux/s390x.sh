a4e6f859ce3850fc164d5069d3a7c5e328cbf1c9
Jay Rodgers <headmelted@gmail.com>
2019-02-14 14:50:48 +0000

    Update s390x.sh

--- kitchen/env/linux/s390x.sh
+++ kitchen/env/linux/s390x.sh
@@ -3,11 +3,11 @@ export ARCHIE_CROSS_TOOLCHAIN="true";
 export ARCHIE_ARCH="s390x";
 export ARCHIE_RPM_ARCH="s390x";
 export ARCHIE_GNU_TRIPLET="s390x-linux-gnu";
+export ARCHIE_HEADERS_GNU_TRIPLET="s390x-linux-gnu";
 export ARCHIE_GNU_MULTILIB_TRIPLET="";
 export ARCHIE_PACKAGE_ARCH="s390x";
 export ARCHIE_QEMU_PACKAGE_ARCH="s390x";
 export ARCHIE_QEMU_ARCH="s390x";
 export ARCHIE_ELECTRON_ARCH="";
-export PKG_CONFIG_PATH="/usr/lib/s390x-linux-gnu/pkgconfig"
 
 export VSCODE_ELECTRON_PLATFORM="";
