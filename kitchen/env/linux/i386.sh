27e287a13da91dd21cf5b70121bcb05067a91e57
Jay Rodgers <headmelted@gmail.com>
2019-02-14 14:50:06 +0000

    Update i386.sh

--- kitchen/env/linux/i386.sh
+++ kitchen/env/linux/i386.sh
@@ -3,12 +3,12 @@ export ARCHIE_CROSS_TOOLCHAIN='true';
 export ARCHIE_ARCH='i386';
 export ARCHIE_RPM_ARCH='x86';
 export ARCHIE_GNU_TRIPLET='i686-linux-gnu';
+export ARCHIE_HEADERS_GNU_TRIPLET='i386-linux-gnu';
 export ARCHIE_GNU_MULTILIB_TRIPLET='';
 export ARCHIE_PACKAGE_ARCH='i386';
 export ARCHIE_QEMU_PACKAGE_ARCH='x86';
 export ARCHIE_QEMU_ARCH='i386';
 export ARCHIE_ELECTRON_ARCH='ia32'; 
-export PKG_CONFIG_PATH="/usr/lib/i386-linux-gnu/pkgconfig"
 
 export VSCODE_ELECTRON_PLATFORM='ia32';
 export npm_config_target_arch='ia32';
