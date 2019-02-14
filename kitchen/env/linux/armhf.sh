0c6718252e506ff5179917082abdfa9652d51270
Jay Rodgers <headmelted@gmail.com>
2019-02-14 14:49:32 +0000

    Update armhf.sh

--- kitchen/env/linux/armhf.sh
+++ kitchen/env/linux/armhf.sh
@@ -3,6 +3,7 @@ export ARCHIE_CROSS_TOOLCHAIN="true";
 export ARCHIE_ARCH="armhf";
 export ARCHIE_RPM_ARCH="armhf";
 export ARCHIE_GNU_TRIPLET="arm-linux-gnueabihf";
+export ARCHIE_HEADERS_GNU_TRIPLET='arm-linux-gnueabihf';
 export ARCHIE_GNU_MULTILIB_TRIPLET="";
 export ARCHIE_PACKAGE_ARCH="arm";
 export ARCHIE_QEMU_PACKAGE_ARCH="arm";
